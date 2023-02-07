2d8440770109a9b0e64e26f2b2b83515e8c9c285
huylesitdn <mrsjiodev@gmail.com>
2023-02-07 11:47:07 +0700

    Homepage-before-login

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -33,7 +33,7 @@ $(function() {
     
   
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 0, // offset (in px) from the original trigger point
+    offset: -200, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 2000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
@@ -189,7 +189,7 @@ $('.universal .play-now a').on("click", function (e) {
 })
 
 
-$('#mySidenav #collapseCountry .collapse__item').on('click', function() {
+$('#myNavbar #collapseCountry .collapse__item').on('click', function() {
   const select_region = $(this).data("region");
   localStorage.setItem(PREFERED_REGION, select_region);
   changeLanguageColor();
@@ -211,9 +211,9 @@ function changeLanguageColor () {
   })
 
   const current_country = translator.translateForKey('menu.Uwin33_' + _get_region, _get_language);
-  $('#mySidenav .current-country').text(current_country);
+  $('#myNavbar .current-country').text(current_country);
   
-  $('#mySidenav #collapseCountry .collapse__item').each(function (){
+  $('#myNavbar #collapseCountry .collapse__item').each(function (){
     const get_attr_region = $(this).data('region');
     if(_get_region == get_attr_region) {
       $(this).addClass('active');
@@ -225,115 +225,6 @@ function changeLanguageColor () {
 
 /**
  * MENU SLIDE
- *
- */
-
-/**
- * SCROLL TEXT
- *
- */
-
-//this is the useful function to scroll a text inside an element...
-function startScrolling(scroller_obj, velocity, start_from) {
-  //bind animation  inside the scroller element
-  scroller_obj
-    .bind("marquee", function (event, c) {
-      //text to scroll
-      var ob = $(this);
-      //scroller width
-      var sw = parseInt(ob.closest(".text-animated").width());
-      //text width
-      var tw = parseInt(ob.width());
-      //text left position relative to the offset parent
-      var tl = parseInt(ob.position().left);
-      //velocity converted to calculate duration
-      var v = velocity > 0 && velocity < 100 ? (100 - velocity) * 1000 : 5000;
-      //same velocity for different text's length in relation with duration
-      var dr = (v * tw) / sw + v;
-      //is it scrolling from right or left?
-      switch (start_from) {
-        case "right":
-          //   console.log('here')
-          //is it the first time?
-          if (typeof c == "undefined") {
-            //if yes, start from the absolute right
-            ob.css({
-              left: sw,
-            });
-            sw = -tw;
-          } else {
-            //else calculate destination position
-            sw = tl - (tw + sw);
-          }
-          break;
-        default:
-          if (typeof c == "undefined") {
-            //start from the absolute left
-            ob.css({
-              left: -tw,
-            });
-          } else {
-            //else calculate destination position
-            sw += tl + tw;
-          }
-      }
-      //attach animation to scroller element and start it by a trigger
-      ob.animate(
-        {
-          left: sw,
-        },
-        {
-          duration: dr,
-          easing: "linear",
-          complete: function () {
-            ob.trigger("marquee");
-          },
-          step: function () {
-            //check if scroller limits are reached
-            if (start_from == "right") {
-              if (parseInt(ob.position().left) < -parseInt(ob.width())) {
-                //we need to stop and restart animation
-                ob.stop();
-                ob.trigger("marquee");
-              }
-            } else {
-              if (
-                parseInt(ob.position().left) > parseInt(ob.parent().width())
-              ) {
-                ob.stop();
-                ob.trigger("marquee");
-              }
-            }
-          },
-        }
-      );
-    })
-    .trigger("marquee");
-  //pause scrolling animation on mouse over
-  scroller_obj.mouseover(function () {
-    $(this).stop();
-  });
-  //resume scrolling animation on mouse out
-  scroller_obj.mouseout(function () {
-    $(this).trigger("marquee", ["resume"]);
-  });
-}
-
-$(function () {
-  $(".text-animated").each(function (i, obj) {
-    if ($(this).find(".text-overflow").width() > $(this).width()) {
-      //settings to pass to function
-      var scroller = $(this).find(".text-overflow"); // element(s) to scroll
-      var scrolling_velocity = 95; // 1-99
-      var scrolling_from = "right"; // 'right' or 'left'
-      //call the function and start to scroll..
-      startScrolling(scroller, scrolling_velocity, scrolling_from);
-    }
-  });
-});
-
-/**
- * END SCROLL TEXT
  *
  */
 
@@ -827,16 +718,16 @@ $(document).ready(function () {
   });
 });
 
-$('.show-action').on('click', function (e) {
-  e.preventDefault();
-  const hasActive = $(this).hasClass('active');
-  $('.show-action').removeClass('active');
-  if (!!hasActive) {
-    $(this).removeClass('active');
-  } else {
-    $(this).addClass('active');
-  }
-})
+// $('.show-action').on('click', function (e) {
+//   e.preventDefault();
+//   const hasActive = $(this).hasClass('active');
+//   $('.show-action').removeClass('active');
+//   if (!!hasActive) {
+//     $(this).removeClass('active');
+//   } else {
+//     $(this).addClass('active');
+//   }
+// })
 
 $('.category-page .btn-left').on('click', function (e) {
   e.preventDefault();
